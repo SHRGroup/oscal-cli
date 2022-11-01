1de5f263b9ca9fc26d1b741708b5f600969e5465
Matthew Ruge <matthew.ruge@shrgroupllc.com>
2022-11-01 13:50:08 -0400

    remove json

--- .github/scripts/check_versions.py
+++ .github/scripts/check_versions.py
@@ -12,16 +12,13 @@ releases = {t.name:t.commit.sha for t in list(nist_tags)}
 target_name = max(releases.keys())
 target_hash = releases.get(target_name)
 
-import requests, base64, json
+import requests, base64
 headers = {"Authorization": f"Bearer {base64.b64encode(token_enc).decode('ascii')}"}
 repo = os.environ.get('TARGET_REPO', "shrgroup/oscal-cli")
 tags = requests.get(f'https://ghcr.io/v2/{repo}/tags/list', headers=headers).json().get('tags')
 tags = [t for t in tags if not t.endswith('.sig')]
-
-if target_hash not in tags:
-    output=dict(name=target_name, hash=target_hash)
-else:
-    output = {}
 print(output)
 with open(os.environ.get('GITHUB_OUTPUT', 'tobuild.json'), 'a') as f:
-    f.write(f"versions=\"{json.dumps(output)}\"")
+    f.write(f"build={str(target_hash not in tags)}")
+    f.write(f"target_name={target_name}")
+    f.write(f"target_hash={target_hash}")
